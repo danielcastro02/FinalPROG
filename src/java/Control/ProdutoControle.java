19f29a5cfbad9ca40819018b883ddd115c357b0f
Daniel <Daniel@200.132.17.123>
2019-05-26 18:00:11 -0300

    Objeto Produto

--- src/java/Control/ProdutoControle.java
+++ src/java/Control/ProdutoControle.java
@@ -7,10 +7,12 @@ package Control;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  *
@@ -29,18 +31,20 @@ public class ProdutoControle extends HttpServlet {
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet ProdutoControle</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet ProdutoControle at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
+            HttpSession session = request.getSession();
+            RequestDispatcher disp = request.getRequestDispatcher("");
+            String funcao = request.getParameter("action");
+            switch (funcao) {
+                case "cadastro":
+                    break;
+                case "addCarrinho":
+                    break;
+                case "removeCarrinho":
+                    break;
+                case "compra":
+                    break;
+            }
         }
     }
 
